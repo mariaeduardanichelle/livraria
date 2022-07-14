bca3db9fd04d4f9aa5ab19fc9be8bc0d90a0f061
Maria Eduarda Nichelle <madunichelle@gmail.com>
2022-07-14 12:08:46 -0300

    add editoras, livros e autores/ aula 14/07/22

--- core/serializers.py
+++ core/serializers.py
@@ -2,10 +2,24 @@ from dataclasses import field
 from pyexpat import model
 from rest_framework.serializers import ModelSerializer
 
-from core.models import Categoria
+from core.models import Autor, Categoria, Editora, Livro
 
+class AutorSerializer(ModelSerializer):
+    class Meta:
+        model = Autor
+        fields = "__all__"
+        
 class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
         fields = "__all__"
         
+class EditoraSerializer(ModelSerializer):
+    class Meta:
+        model = Editora
+        fields = "__all__"
+
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
