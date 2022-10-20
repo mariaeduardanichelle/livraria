8f02588c0049558d67ed7ce8a379f7fb2f1b1659
mariaeduardanichelle <madunichelle@gmail.com>
2022-10-20 13:28:41 -0300

    aula 17

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 from core.models import Autor, Categoria, Editora, Livro
 
 admin.site.register(Autor)
@@ -7,3 +8,23 @@ admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Livro)
 # Register your models here.
+class UsuarioAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "cpf", "telefone", "data_nascimento")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+admin.site.register(Usuario, UsuarioAdmin)
