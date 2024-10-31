4421ed2d680e7e3677cff3c8986ba2ee644fadc3
mariaeduardanichelle <madunichelle@gmail.com>
2024-10-31 00:20:15 -0300

    feat: Dois serializadores para Livro

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -2,4 +2,4 @@ from .autor import AutorSerializer
 from .categoria import CategoriaSerializer 
 from .editora import EditoraSerializer
 from .user import UserSerializer
-from .livro import LivroSerializer
+from .livro import LivroDetailSerializer, LivroSerializer, LivroListSerializer
--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -2,7 +2,19 @@ from rest_framework.serializers import ModelSerializer
 
 from core.models import Livro
 
-class LivroSerializer(ModelSerializer): #1
-    class Meta: 
-        model = Livro 
-        fields = "__all__" 
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+
+
+class LivroDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = "__all__"
+        depth = 1
+
+class LivroListSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = ("id", "titulo", "preco")
