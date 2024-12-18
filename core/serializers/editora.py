60d61aa225cf653f20dd4f160c57a7548b983c3a
mariaeduardanichelle <madunichelle@gmail.com>
2024-12-18 14:45:24 -0300

    feat: formatando dados antes de salvar

--- core/serializers/editora.py
+++ core/serializers/editora.py
@@ -3,6 +3,9 @@ from rest_framework.serializers import ModelSerializer
 from core.models import Categoria, Editora
 
 class EditoraSerializer(ModelSerializer):
+    def validate_email(self, email):
+        return email.lower()
+        
     class Meta:
         model = Editora
         fields = "__all__"
