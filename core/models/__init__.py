a6dc6d214a3669bdcfe1d0c0d91cf42537964705
mariaeduardanichelle <madunichelle@gmail.com>
2025-02-02 20:11:18 -0300

    featÇ adicionando favoritos

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,5 +2,6 @@ from .autor import Autor
 from .user import User
 from .categoria import Categoria
 from .editora import Editora
+from .favoritos import Favoritos
 from .livro import Livro
 from .compra import Compra, ItensCompra
